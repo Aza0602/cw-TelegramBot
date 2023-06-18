982b8c22a030ff36ae492b677a168a6c7d5e716e
Azat <aelchikushuulu@gmail.com>
2023-06-18 15:53:46 +0600

    Initial commit

---
+++ src/main/java/pro/sky/cw3/CwTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwTelegramBotApplication.class, args);
+    }
+
+}
